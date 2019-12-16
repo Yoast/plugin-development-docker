0867499d897157fff46a3fda69b2ef1be729fd46
Herre Groen <herre@yoast.com>
2019-12-16 14:21:24 +0100

    Minor improvements to start script

--- start.sh
+++ start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-SCRIPTS_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-cd "$SCRIPTS_PATH/../docker"
-
 trap stop_docker INT
 function stop_docker {
-    STOPPING=true
+	STOPPING=true
 	wait $PROCESS
 	exit
 }
@@ -16,20 +12,34 @@ docker-compose up --no-start
 echo "Booting containers"
 docker-compose up &
 PROCESS=$!
-sleep 2
+
+docker exec -ti local-wordpress /bin/bash -c "usermod -u $(id -u) www-data"
+docker exec -ti local-wordpress /bin/bash -c "groupmod -g $(id -g) www-data"
+docker restart local-wordpress
+
+echo "Waiting for containters to boot..."
+while [ "$BOOTED" != "true"  ]; do
+	if curl -I http://local.wordpress.test 2>/dev/null | grep -q "HTTP/1.1 200 OK"; then
+		BOOTED=true
+	else
+		sleep 2
+		echo "Waiting for containters to boot..."
+	fi
+done
 
 open "http://local.wordpress.test" 2>/dev/null || x-www-browser "http://local.wordpress.test"
 
-while true; do
+while [ "$STOPPING" != 'true' ]; do
 	CLOCK_SOURCE=`docker exec -ti local-wordpress /bin/bash -c 'cat /sys/devices/system/clocksource/clocksource0/current_clocksource'| tr -d '[:space:]'`
-	if [[ "$CLOCK_SOURCE" != 'tsc' && "$STOPPING" != "true" ]]; then
+	if [[ "$CLOCK_SOURCE" != 'tsc' && "$STOPPING" != 'true' ]]; then
 		echo "Restarting docker now to fix out-of-sync hardware clock!"
 		docker ps -q | xargs -L1 docker stop
 		osascript -e 'quit app "Docker"'
 		open --background -a Docker
 		echo "Giving docker time to start..."
 		until docker info 2> /dev/null 1> /dev/null; do
-			sleep 1
+			sleep 2
+			echo "Giving docker time to start..."
 		done
 		echo "Docker is up and running again! Booting containers!"
 		docker-compose up &
