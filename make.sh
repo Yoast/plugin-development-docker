128a4937bc86d05a4081df2b7a8c2747e836559a
Herre Groen <herre@yoast.com>
2020-01-09 13:49:38 +0100

    Fix woocommerce hostname in make.sh

--- make.sh
+++ make.sh
@@ -34,7 +34,7 @@ change_hostfile () {
 source ./config/config.sh
 
 change_hostfile ${BASIC_HOST:-basic.wordpress.test}
-change_hostfile ${WOOCOMMERCE_HOST:-woocomerce.wordpress.test}
+change_hostfile ${WOOCOMMERCE_HOST:-woocommerce.wordpress.test}
 change_hostfile ${MULTISITE_HOST:-multisite.wordpress.test}
 change_hostfile ${BASIC_DATABASE_HOST:-basic-database.wordpress.test}
 change_hostfile ${WOOCOMMERCE_DATABASE_HOST:-woocommerce-database.wordpress.test}
