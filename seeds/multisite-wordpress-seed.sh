0b07659bb307d234d3270df6ba04ab90e2fd1ce2
Herre Groen <herre@yoast.com>
2020-02-03 11:48:08 +0100

    Fix multisite installation and seed it with data

--- seeds/multisite-wordpress-seed.sh
+++ seeds/multisite-wordpress-seed.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-source "${BASH_SOURCE%/*}/core-install.sh"
-wp core multisite-convert
+wp core multisite-install --url=${SITE_URL} --title=${SITE_TITLE} --admin_user=${ADMIN_USERNAME} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL}
+wp rewrite structure "%postname%/"
+wp rewrite flush --hard
+wp plugin install debug-bar --activate
+wp plugin install query-monitor --activate
 wp super-admin add admin
 wp site create --slug=site2
+wp faker core content
+wp faker core content --url=${SITE_URL}/site2
