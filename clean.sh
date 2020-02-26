21647cd7db51a8a66637ef274d471b9ef0aa4718
Jannes <jannes@yoast.com>
2020-02-26 11:34:48 +0100

    Add --all argument to clean.sh to remove configs

--- clean.sh
+++ clean.sh
@@ -4,3 +4,11 @@ docker-compose stop
 docker-compose rm -fv
 rm -rf wordpress
 git checkout -- wordpress/.gitkeep
+
+case $1 in
+    -a|--all)
+        echo "Option '--all' defined. Removing non-default config files."
+        rm -f ./config/config.sh
+        rm -f ./config/php.ini
+        ;;
+esac
