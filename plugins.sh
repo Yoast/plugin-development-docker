b2c8b887eb619d3a199b2c2e5c03e8ab4f5e1368
Jip <jhp.moors@gmail.com>
2020-07-08 11:14:21 +0200

    Install from WordPress.org or Github
    Providing a Github link will allow for installing of plugins via Github.
    
    This will execute `composer` and `yarn` install commands if the config files for those are found.

--- plugins.sh
+++ plugins.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
-declare -a PluginList=("query-monitor" "user-switching" "yoast-test-helper")
+function install_github_plugin {
+  plugin=$1
+  slug=${plugin##*/}
 
-# Install each plugin
+  if [ ! -d "./plugins/$slug" ]; then
+    cd plugins
+    $(echo git clone $plugin $slug)
+    cd -
+  fi
+
+  cd plugins/$slug/
+
+  if [ -f "composer.json" ]; then
+    composer install
+  fi
+
+  if [ -f "package.json" ]; then
+    yarn
+  fi
+
+  cd -
+
+  $(echo ./wp.sh plugin install $slug --activate)
+}
+
+declare -a PluginList=("query-monitor" "user-switching" "https://github.com/Yoast/yoast-test-helper")
+
 for plugin in "${PluginList[@]}"; do
 	echo Installing $plugin.
-	if [ -d "./plugins/$plugin/.git" ]; then
-		echo "Git clone found, not installing from WordPress.org."
-	else 
-		$(echo ./wp.sh plugin install $plugin --activate)
+  if [[ $plugin == "https://github.com"* ]]; then
+    install_github_plugin $plugin
+  else
+    if [ -d "./plugins/$plugin/.git" ]; then
+		  echo "Git clone found, not installing from WordPress.org."
+	  else
+  		$(echo ./wp.sh plugin install $plugin --activate)
+	  fi
 	fi
 done
-
