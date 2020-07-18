0762ece9184ad856784b1e23d99dd6e928f0d565
Jip Moors <jhp.moors@gmail.com>
2020-07-18 23:00:41 +0200

    Configure www-data user/group in Dockerfile Removed the browser bootup from start script. Removed the www-data user/group config from the WordPress install portion. Created a template which is copied and filled in on start.

--- start.sh
+++ start.sh
@@ -14,29 +14,35 @@ if [[ -z "$@" ]]; then
 else
 	CONTAINERS="$@"
 fi
+
 URL_basic_wordpress="http://${BASIC_HOST:-basic.wordpress.test}"
 URL_woocommerce_wordpress="http://${WOOCOMMERCE_HOST:-woocommerce.wordpress.test}"
 URL_multisite_wordpress="http://${MULTISITE_HOST:-multisite.wordpress.test}"
 URL_standalone_wordpress="http://${STANDALONE_HOST:-standalone.wordpress.test}"
 
-echo "Starting containers:"
-for CONTAINER in $CONTAINERS; do
-	echo "  - $CONTAINER"
-done
-echo "Ensuring all containers are built"
-docker-compose build --pull --parallel $CONTAINERS
-
-USER_ID=`id -u`
-GROUP_ID=`id -g`
-
-echo "Booting containers"
-docker-compose up --detach $CONTAINERS
-
 PORT_basic_wordpress=1987
 PORT_woocommerce_wordpress=1988
 PORT_multisite_wordpress=1989
 PORT_standalone_wordpress=1990
-# First wait for the DBs to boot.
+
+USER_ID=`id -u`
+GROUP_ID=`id -g`
+
+cp containers/wordpress/Dockerfile.template containers/wordpress/Dockerfile
+sed -i -e "s/\$UID/$USER_ID/g" containers/wordpress/Dockerfile
+sed -i -e "s/\$GID/$GROUP_ID/g" containers/wordpress/Dockerfile
+
+echo "Starting containers:"
+for CONTAINER in $CONTAINERS; do
+	echo "  - $CONTAINER"
+done
+
+echo "Ensuring all containers are built..."
+docker-compose build --pull --parallel $CONTAINERS
+
+echo "Booting containers..."
+docker-compose up --detach $CONTAINERS
+
 echo "Waiting for databases to boot..."
 for CONTAINER in $CONTAINERS; do
 	PORT_VAR="PORT_${CONTAINER//-/_}"
@@ -49,21 +55,20 @@ done
 
 # Then install WordPress.
 for CONTAINER in $CONTAINERS; do
-	echo "Checking if $CONTAINER is installed!"
-	docker exec -ti $CONTAINER /bin/bash -c 'until [[ -f .htaccess ]]; do sleep 1; done'
-	docker exec -ti $CONTAINER /bin/bash -c 'wp --allow-root core is-installed 2>/dev/null'
+	echo "Checking if WordPress is installed in $CONTAINER..."
+
+	docker exec -ti "$CONTAINER" /bin/bash -c 'until [[ -f .htaccess ]]; do sleep 1; done'
+	docker exec -ti "$CONTAINER" /bin/bash -c 'wp --allow-root core is-installed 2>/dev/null'
 	IS_INSTALLED=$?
 
-	docker exec -ti $CONTAINER /bin/bash -c "usermod -u ${USER_ID} www-data"
-	docker exec -ti $CONTAINER /bin/bash -c "groupmod -o -g ${GROUP_ID} www-data"
-	docker container restart $CONTAINER
-
+	# 1 == not installed (exit code)
 	if [ $IS_INSTALLED == 1 ]; then
-		echo "Installing WordPress for $CONTAINER"
-		docker exec -ti $CONTAINER /bin/bash -c 'mkdir -p /var/www/.wp-cli/packages; chown -R www-data:www-data /var/www;'
-		docker exec --user $USER_ID -ti $CONTAINER /bin/bash -c 'php -d memory_limit=512M "$(which wp)" package install git@github.com:Yoast/wp-cli-faker.git'
-		docker cp ./seeds $CONTAINER:/seeds
-		docker exec --user $USER_ID -ti $CONTAINER /seeds/$CONTAINER-seed.sh
+		echo "Installing WordPress for $CONTAINER..."
+
+		docker exec -ti "$CONTAINER" /bin/bash -c 'mkdir -p /var/www/.wp-cli/packages; chown -R www-data: /var/www;'
+		docker exec --user "$USER_ID" -ti "$CONTAINER" /bin/bash -c 'php -d memory_limit=512M "$(which wp)" package install git@github.com:Yoast/wp-cli-faker.git'
+		docker cp ./seeds "$CONTAINER":/seeds
+		docker exec --user "$USER_ID" -ti "$CONTAINER" /seeds/"$CONTAINER"-seed.sh
 	fi
 done
 
@@ -72,7 +77,7 @@ for CONTAINER in $CONTAINERS; do
 	URL_VAR="URL_${CONTAINER//-/_}"
 	URL=${!URL_VAR}
 	while [ "$BOOTED" != "true"  ]; do
-		if curl -I $URL 2>/dev/null | grep -q -e "HTTP/1.1 200 OK" -e "HTTP/1.1 302 Found"; then
+		if curl -I "$URL" 2>/dev/null | grep -q -e "HTTP/1.1 200 OK" -e "HTTP/1.1 302 Found"; then
 			BOOTED=true
 		else
 			sleep 2
@@ -81,20 +86,13 @@ for CONTAINER in $CONTAINERS; do
 	done
 done
 
-for CONTAINER in $CONTAINERS; do
-	URL_VAR="URL_${CONTAINER//-/_}"
-	echo "Starting ${!URL_VAR}"
-	open ${!URL_VAR} 2>/dev/null || x-www-browser ${!URL_VAR} &
-	break
-done
-
 echo "Containers have booted! Happy developing!"
 echo "Outputting logs now:"
 docker-compose logs -f &
 PROCESS=$!
 
 while [ "$STOPPING" != 'true' ]; do
-	CLOCK_SOURCE=`docker exec -ti nginx-router-wordpress /bin/bash -c 'cat /sys/devices/system/clocksource/clocksource0/current_clocksource' | tr -d '[:space:]'`
+	CLOCK_SOURCE=$(docker exec -ti nginx-router-wordpress /bin/bash -c 'cat /sys/devices/system/clocksource/clocksource0/current_clocksource' | tr -d '[:space:]')
 	if [[ "$CLOCK_SOURCE" != 'tsc' && "$CLOCK_SOURCE" != 'hyperv_clocksource_tsc_page' && "$STOPPING" != 'true' ]]; then
 		echo "Restarting docker now to fix out-of-sync hardware clock!"
 		docker ps -q | xargs -L1 docker stop
@@ -106,7 +104,7 @@ while [ "$STOPPING" != 'true' ]; do
 			echo "Giving docker time to start..."
 		done
 		echo "Docker is up and running again! Booting containers!"
-		docker-compose up --detach $CONTAINERS
+		docker-compose up --detach "$CONTAINERS"
 	fi
 	sleep 5
 done
