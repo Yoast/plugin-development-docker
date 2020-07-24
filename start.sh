c40ae25770f3e6d85c95873546df1a25da675d3c
Jannes <jannes@yoast.com>
2020-07-24 15:41:09 +0200

    Fix sed for Mac OS

--- start.sh
+++ start.sh
@@ -35,9 +35,9 @@ USER_ID=`id -u`
 GROUP_ID=`id -g`
 
 if [ ! -f './containers/wordpress/Dockerfile' ]; then
-	cp containers/wordpress/Dockerfile.template containers/wordpress/Dockerfile
-	sed -i -e "s/\$UID/${USER_ID}/g" containers/wordpress/Dockerfile
-	sed -i -e "s/\$GID/${GROUP_ID}/g" containers/wordpress/Dockerfile
+	cp ./ './containers/wordpress/Dockerfile.template' './containers/wordpress/Dockerfile'
+	sed -i '' -e "s/\$UID/${USER_ID}/g" './containers/wordpress/Dockerfile'
+	sed -i '' -e "s/\$GID/${GROUP_ID}/g" './containers/wordpress/Dockerfile'
 fi
 
 echo "Starting containers:"
