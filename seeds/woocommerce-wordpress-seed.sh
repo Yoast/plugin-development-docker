d4292c0c4658ba391f0c3c29768b46fe2040c34b
thijsvanloef <thijsvanloef@yoast.com>
2021-06-23 13:28:05 +0200

    Fixed installation for all sites

--- seeds/woocommerce-wordpress-seed.sh
+++ seeds/woocommerce-wordpress-seed.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-source "${BASH_SOURCE%/*}/core-install.sh"
-wp plugin install woocommerce --activate
-wp plugin install woocommerce-admin --activate
-wp plugin install perfect-woocommerce-brands --activate
-wp theme install storefront --activate
-wp wc tool run install_pages --user=admin
+echo "$(source "${BASH_SOURCE%/*}/core-install.sh")"
+echo "$(wp plugin install woocommerce --activate)"
+echo "$(wp plugin install woocommerce-admin --activate)"
+echo "$(wp plugin install perfect-woocommerce-brands --activate)"
+echo "$(wp theme install storefront --activate)"
+echo "$(wp wc tool run install_pages --user=admin)"
 wp option add storefront_nux_dismissed true
-wp user meta add 1 dismissed_install_notice true
-wp post update 1 --post_type=page --post_title=Homepage --comment-status=closed --post_content='<!-- wp:heading {"align":"center"} -->
+echo "$(wp user meta add 1 dismissed_install_notice true)"
+echo "$(wp post update 1 --post_type=page --post_title=Homepage --comment-status=closed --post_content='<!-- wp:heading {"align":"center"} -->
 <h2 class="has-text-align-center">Shop by Category</h2>
 <!-- /wp:heading -->
 
@@ -37,7 +37,7 @@ wp post update 1 --post_type=page --post_title=Homepage --comment-status=closed 
 <h2 class="has-text-align-center">Best Sellers</h2>
 <!-- /wp:heading -->
 
-<!-- wp:woocommerce/product-best-sellers {"columns":4} /-->'
+<!-- wp:woocommerce/product-best-sellers {"columns":4} /-->')"
 wp option update show_on_front page
 wp option update page_on_front 1
 wp faker woocommerce products
