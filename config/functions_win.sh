b2a19fc135f27fc232425611e6a9ca6618786959
noud-github <arnoud@yoast.com>
2022-08-16 14:09:40 +0200

    add setup pass to windows setup

--- config/functions_win.sh
+++ config/functions_win.sh
@@ -4,6 +4,20 @@
 hostfile=/mnt/c/Windows/System32/drivers/etc/hosts
 
 #######################################
+# Function setup pass needed for docker pulling
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function setup_pass() {
+	sudo apt-get update
+	sudo apt-get -y install pass
+}
+
+#######################################
 # Function that groups make tasks
 # Globals:
 #   None
@@ -14,6 +28,7 @@ hostfile=/mnt/c/Windows/System32/drivers/etc/hosts
 #######################################
 function platform_setup() {
 	platform_independent_make $hostfile
+	setup_pass
 }
 
 
